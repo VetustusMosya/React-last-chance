216d185b4b489176b58e538d282d917bdb231c53
Arseni Maiseyeu <moiseevsenya2002@gmail.com>
2024-12-21 21:04:49 +0300

    button by styled component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,13 +1,11 @@
-import "./Button.css";
+import styled from "styled-components";
 
-export const Button = ({ className, onClick, type, children }) => {
-  return (
-    <button
-      className={(className ?? "") + " button"}
-      onClick={onClick}
-      type={type}
-    >
-      {children}
-    </button>
-  );
-};
+export const Button = styled.button`
+  align-self: center;
+  max-width: 300px;
+  padding: 0.3rem 3rem;
+  font-weight: 600;
+  background-color: #121212;
+  color: antiquewhite;
+  border-radius: 0.1rem;
+`;
